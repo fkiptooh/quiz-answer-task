af88d13f39497e3d5337e29bb82cacf35c814ff3
fkiptooh <fkiptooh@gmail.com>
2023-10-25 23:35:05 +0300

    assignment setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+interface QuestionProps {
+  question: string;
+  correctAnswer: string;
+  choices: string[];
+}
+
+const question: QuestionProps[] = [
+  {
+    question: "What is the color of the sky?",
+    correctAnswer: "Blue",
+    choices: ["Blue", "Red", "White", "Pink"],
+  },
+];
+const App = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  const currentQuestion = question[currentQuestionIndex];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <h1>{currentQuestion.question}</h1>
+        {currentQuestion.choices.map((answer) => (
+          <label>
+            <input type="checkbox" name="answer" />
+            {answer}
+          </label>
+        ))}
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
