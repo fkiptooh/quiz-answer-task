a852d10df440b6c2c25468d92be2aae07fe9edda
fkiptooh <fkiptooh@gmail.com>
2023-10-26 12:52:19 +0300

    logic - tracked user answers and gave scorecard at the end as well as added a reset button

--- src/App.tsx
+++ src/App.tsx
@@ -13,26 +13,73 @@ const question: QuestionProps[] = [
     correctAnswer: "Blue",
     choices: ["Blue", "Red", "White", "Pink"],
   },
+  {
+    question: "What is the capital city of Kenya?",
+    correctAnswer: "Nairobi",
+    choices: ["Mombasa", "Nairobi", "Kericho", "Kisumu"],
+  },
+  {
+    question: "What is the capital city of Uganda?",
+    correctAnswer: "Kampala",
+    choices: ["Dodoma", "Kigali", "Djbouti", "Kampala"],
+  },
 ];
 const App = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [score, setScore] = useState(0);
 
   const currentQuestion = question[currentQuestionIndex];
-  return (
-    <div>
-      <form>
-        <h1>{currentQuestion.question}</h1>
-        {currentQuestion.choices.map((answer) => (
-          <label>
-            <input type="checkbox" name="answer" />
-            {answer}
-          </label>
-        ))}
 
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+  const isGameOver = currentQuestionIndex === question.length;
+
+  const handleReset = () => {
+    setScore(0);
+    setCurrentQuestionIndex(0);
+    setSelectedAnswer("");
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (selectedAnswer === currentQuestion.correctAnswer) {
+      setScore(score + 1);
+    }
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
+
+  const Quiz = () => {
+    return (
+      <>
+        <h1>{currentQuestion.question}</h1>
+        <form className="form-quiz" onSubmit={handleSubmit}>
+          {currentQuestion.choices.map((answer) => (
+            <label key={answer}>
+              <input
+                type="radio"
+                name="answer"
+                checked={answer === selectedAnswer}
+                onChange={() => setSelectedAnswer(answer)}
+              />
+              {answer}
+            </label>
+          ))}
+
+          <button>Submit</button>
+        </form>
+      </>
+    );
+  };
+
+  const ScoreCard = () => {
+    const wrongAttemps = question.length - score;
+    return (
+      <div className="form-quiz">
+        Your got {score} correct, you got {wrongAttemps} questions wrong{" "}
+        <button onClick={handleReset}>Try again</button>
+      </div>
+    );
+  };
+  return <div>{isGameOver ? <ScoreCard /> : <Quiz />} </div>;
 };
 
 export default App;
